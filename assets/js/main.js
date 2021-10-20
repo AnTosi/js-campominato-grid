c9ce8ecf7d22755fae85b688c7ff15d536ab3da1
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-20 16:27:34 +0200

    ok, finalmente il ciclo for funziona

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,21 +6,50 @@
 
 //3 bottoni con event listener per far scegliere la difficoltà e scegliere quale ciclo innescare
 //3 cicli uno per difficoltà vincolati all if - bottone cliccato per generare la griglia con innerHTML, append o insertAdjacentHTML, dovrebbero funzionare tutti e 3
+//essendo 3 cicli uguali farò una funzione
 //per i numeri delle celle scriverò dentro alla cella stessa l'i del ciclo nel processo di generazione
 //eventListener (click, ) nelle celle per colorarle con il click
 
 // comincio con i 3 bottoni di generazione della griglia
 // ok li ho creati in HTML
 
-let easyClick = document.getElementById("buttOne").addEventListener("click", function () {
-    console.log("ez");
-  }); 
+let easyClick = document.getElementById("buttOne").addEventListener("click", function generateGrid(easy, easyN){
+
+}); 
 
 let mediumClick = document.getElementById("buttTwo").addEventListener("click", function () {
-    console.log("meh");
+    return true
 }); 
 
 let hardClick = document.getElementById("buttThree").addEventListener("click", function () {
-    console.log("ora si comincia a ragionare");
+    return true
 }); 
 
+
+const easyN = 49;
+const mediumN = 81;
+const hardN = 100;
+
+const easy = "easy";
+const medium = "medium";
+const hard = "hard";
+
+//ora creo funzione, prima funzione normale poi se funziona inserisco anche reset
+
+function generateGrid (difficulty, cellN) {
+    for (let i = 1; i < cellN; i++) {
+        document.querySelector(".container").insertAdjacentHTML("beforeend") = `<div class= "cell" + ${difficulty}">${i}</div>`;
+    }
+}
+
+
+
+// ho riscontrato problemi a inserire la funzione nell'event listener (non mi prende i parametri), ora provo a mettere gli if: 
+
+// function generateGrid (easy, easyN);
+
+
+
+    for (let i = 1; i < easyN + 1; i++) {
+        document.querySelector(".container").insertAdjacentHTML("beforeend", `<div class= "cell ${easy}">${i}</div>`);
+    }
