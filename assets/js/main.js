2a5b800287e29d9c313a7f30329213f746f8d26f
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-20 15:22:09 +0200

    scritta traccia e modus operandi

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// chiedo all'utente la difficoltà (magari con un bottone? in tal caso dovrò impostare un reset prima della creazione della nuova griglia)
+// in base alla difficoltà scelta genero griglia quadrata con ogni cella un numero in un range
+// quando l'utente clicca sulla casella (addEventListener) si colora di azzurro
+
+//strumenti
+
+//3 bottoni con event listener per far scegliere la difficoltà e scegliere quale ciclo innescare
+//3 cicli uno per difficoltà vincolati all if - bottone cliccato per generare la griglia con innerHTML, append o insertAdjacentHTML, dovrebbero funzionare tutti e 3
+//per i numeri delle celle scriverò dentro alla cella stessa l'i del ciclo nel processo di generazione
+//eventListener (click, ) nelle celle per colorarle con il click
+
+// comincio con i 3 bottoni di generazione della griglia
